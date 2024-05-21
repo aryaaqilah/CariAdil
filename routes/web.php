8db307e63a4a04a2be11ad1e752859728efb26f3
easss29 <andreaswiranata29@gmail.com>
2024-05-21 15:30:33 +0700

    eas

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CaseController;
+use App\Http\Controllers\DonationController;
+use App\Http\Controllers\FormController;
 
-Route::get('/', function () {
-    return "andreas";
-});
-
-// /store
-Route::get('/store', function(){
-    return "store";
-});
-
-// store/id-barang
-
-// /kasus-hukum
-// /kasus-hukum/id-kasus
-// /kasus-hukum/id/donasi
-// /form-pengajuan-hukum
-
-// /lbh/login
-// /lbh/pengajuan-bantuan-hukum
-// /lbh/pengajuan-bantuan-hukum/id-kasus
-// /lbh/perkara-berlangsung
-// /lbh/perkara-berlangsung/id-kasus
-Route::prefix('/lbh')->group(function(){
-    Route::get('/login', function(){
-        return "login";
-    });
-
-    Route::get('/pengajuan-bantuan-hukum/{id_kasus?}', function($id_kasus){
-        return 'a';
-    });
-});
-
-// /admin/login
-// /admin/dashboard
-// /admin/role
-// /admin/donasi
-// /admin/produk
-// /admin/kasus-hukum
-// /admin/pengajuan
-// /admin/pengajuan/edit
+Route::any('/store/{id_barang}', []);
+Route::any('/kasus-hukum', []);
+Route::any('/kasus-hukum/{id_kasus}', []);
+Route::any('/kasus-hukum/{id_kasus}/donasi', []);
+Route::any('/form-pengajuan-hukum', []);
