155b6a1356150638659bd1715004d87d4eaec9dd
diandrakina <diandrapramestikinasih@gmail.com>
2024-05-21 15:08:50 +0700

    Route Making

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,30 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "andreas";
 });
+
+// /store
+Route::get('/store', function(){
+    return "store";
+});
+
+// store/id-barang
+
+// /kasus-hukum
+// /kasus-hukum/id-kasus
+// /kasus-hukum/id/donasi
+// /form-pengajuan-hukum
+// /lbh/login
+// /lbh/pengajuan-bantuan-hukum
+// /lbh/pengajuan-bantuan-hukum/id-kasus
+// /lbh/perkara-berlangsung
+// /lbh/perkara-berlangsung/id-kasus
+// /admin/login
+// /admin/dashboard
+// /admin/role
+// /admin/donasi
+// /admin/produk
+// /admin/kasus-hukum
+// /admin/pengajuan
+// /admin/pengajuan/edit
