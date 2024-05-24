5d30b64d5ff9face46f9a875d1a7e4b3dd8cff04
Evotianus <evotianus0110@gmail.com>
2024-05-25 05:05:14 +0700

    Adding homepage kasus hukum controller

--- app/Http/Controllers/KasusHukumController.php
+++ app/Http/Controllers/KasusHukumController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\KasusHukum;
 
 class KasusHukumController extends Controller
 {
-    //
+    public function kasusTerbaruHomepage() {
+        $kasusHukum = KasusHukum::orderBy('created_at')->take(2)->get();
+
+        if ($kasusHukum->isEmpty()) {
+            return 'No data';
+        }
+
+        return $kasusHukum;
+    }
+
+    public function kasusHukumHomepage() {
+
+    }
 }
