7b65f137bc2f5ab062f4a604b7ade957b529ff99
diandrakina <diandrapramestikinasih@gmail.com>
2024-05-27 11:29:24 +0700

    Memindahkan navbar ke core

---
+++ resources/views/core/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="topnav">
+    <div>
+        <img src="assets/images/Logo CariAdil.png" class="logo" alt="Logo CariAdil">
+    </div>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Berita</a></li>
+        <li><a href="#">Laporan</a></li>
+        <li><a href="#">Tentang</a></li>
+    </ul>
+</nav>
