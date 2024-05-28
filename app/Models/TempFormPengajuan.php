102c4bf337fa4de35ee6eb83bc989cd6b4bdbeb3
naulitaA <naulitarns@gmail.com>
2024-05-28 14:36:08 +0700

    Migrate TempFormPengajuan
    Untuk form pengajuan yang disubmit user, masuk ke admin, sebelum admin verifikasi

---
+++ app/Models/TempFormPengajuan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TempFormPengajuan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'temp_form_pengajuan';
+    protected $guarded = [];
+}
