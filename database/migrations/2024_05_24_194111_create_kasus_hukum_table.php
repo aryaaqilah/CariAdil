7cfb211f2d4a6a44ab1121ab6f986bfe58499c51
aryaaqilah <111030604+aryaaqilah@users.noreply.github.com>
2024-07-07 14:20:25 +0700

    bikin dummy data + modif kasus hukum pake idLBH

--- database/migrations/2024_05_24_194111_create_kasus_hukum_table.php
+++ database/migrations/2024_05_24_194111_create_kasus_hukum_table.php
@@ -14,21 +14,21 @@ return new class extends Migration
         Schema::create('kasus_hukum', function (Blueprint $table) {
             $table->id('id_kasus');
             $table->string('title');
-            $table->string('nama_lbh');
             $table->text('description');
             $table->date('tanggal');
             $table->integer('target_donasi');
             $table->bigInteger('id_bank')->unsigned();
             $table->bigInteger('id_form')->unsigned();
-
+            $table->bigInteger('id_lbh')->unsigned();
 
             $table->string('jenis_perkara');
             $table->string('status_pengajuan');
-            
+
             $table->text('image_url');
-            
+
             $table->foreign('id_form')->references('id_form')->on('form_pengajuan')->onDelete('cascade');
             $table->foreign('id_bank')->references('id_bank')->on('bank')->onDelete('cascade');
+            $table->foreign('id_lbh')->references('id_LBH')->on('LBH')->onDelete('cascade');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\Bank;
+use App\Models\FormPengajuan;
+use App\Models\TransaksiDonasi;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\KasusHukum;
+use App\Models\LBH;
+use App\Models\ProgressKasusHukum;
 use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
@@ -21,34 +26,122 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $formArray = [
+            [
+                'nama' => 'arya aqilah',
+                'nama_panggilan' => 'arya',
+                'tempat_lahir' => 'Sukabumi',
+                'jenis_kelamin' => 'Pria',
+                'agama' => 'Islam',
+                'status_perkawinan' => 'Belum Kawin',
+                'alamat_lengkap' => 'Sentul Sentul Sentul',
+                'RT_RW' => '01/04',
+                'Desa_Kelurahan' => 'Sumur Batu',
+                'Kabupaten_Kota' => 'Bogor',
+                'kode_pos' => '43192',
+                'kecamatan' => 'Babakan Madang',
+                'provinsi' => 'Jawa Barat',
+                'identitas_diri' => 'KTP',
+                'nomor_identitas' => '320233',
+                'keterangan_tidak_mampu' => 'Tidak',
+                'nomor_keterangan_tidak_mampu' => '56789',
+                'pekerjaan' => 'mahasiswa',
+                'jumlah_tanggungan' => 2,
+                'pendidikan_terakhir' => 'SMA',
+                'telepon' => 'Tidak Ada',
+                'HP' => '09876',
+                'uraian' => 'Saya adalah seseorang yang terkena masalah',
+                'lokasi_pembuatan_form' => 'Bogor',
+                'tanggal' => Carbon::now(),
+                'image_url' => 'yguhnjmk',
+            ]
+        ];
+
+        $bankArray = [
+            [
+                'nama' => 'BCA',
+                'no_rekening' => '0381160679',
+            ]
+        ];
+
+        $lbhArray = [
+            [
+                'nama_lbh'=> 'Ojan Lomba',
+                'password' => '1234',
+                'nomor_telepon' => '012345',
+                'email' => 'gmail@com',
+                'alamat' => 'Sukabumi'
+            ]
+        ];
 
         $kasusHukumArray = [
             [
-                'title' => 'Ojan Lomba Title Semoga Menang',
-                'nama_lbh' => 'Ojanlomba',
-                'description' => 'Ojan lagi lomba semoga dia bisa menang dan mendapatkan uang jutaan rupiah dipotong pajak',
+                'title' => 'Bantu Pemuda Bogor Ini',
+                'id_lbh' => 1,
+                'description' => 'Kasus ini adalah tentang seorang pemuda bogor',
                 'tanggal' => Carbon::now(),
-                'target_donasi' => 1000000,
-                'image_url' => 'awefiuih',
+                'target_donasi' => '10000000',
+                'id_bank' => 1,
+                'id_form' => 1,
+                'jenis_perkara' => 'Perdata',
+                'status_pengajuan' => 'Diterima',
+                'image_url' => 'tygbh'
+            ]
+        ];
+
+        $progressArray = [
+            [
+                'id_kasus' => 1,
+                'topik_progress' => 'Sidang 1',
+                'date' => Carbon::now(),
+                'detail' => 'Sudah dilaksanakan sidang pertama',
+            ]
+        ];
+
+        $transaksiArray = [
+            [
+                'id_kasus_hukum' => 1,
+                'nominal' => '200000',
             ],
             [
-                'title' => 'Ojan Lomba 2 Title Semoga Menang',
-                'nama_lbh' => 'Ojanlomba 2',
-                'description' => 'Ojan 2 lagi lomba semoga dia bisa menang dan mendapatkan uang jutaan rupiah dipotong pajak',
-                'tanggal' => Carbon::now(),
-                'target_donasi' => 1000000,
-                'image_url' => 'awefiuih',
-            ],
-            [
-                'title' => 'Ojan Lomba 3 Title Semoga Menang',
-                'nama_lbh' => 'Ojanlomba 3',
-                'description' => 'Ojan 3 lagi lomba semoga dia bisa menang dan mendapatkan uang jutaan rupiah dipotong pajak',
-                'tanggal' => Carbon::now(),
-                'target_donasi' => 1000000,
-                'image_url' => 'awefiuih',
+                'id_kasus_hukum' => 1,
+                'nominal' => '300000',
             ],
         ];
 
+        // $kasusHukumArray = [
+        //     [
+        //         'title' => 'Ojan Lomba Title Semoga Menang',
+        //         'nama_lbh' => 'Ojanlomba',
+        //         'description' => 'Ojan lagi lomba semoga dia bisa menang dan mendapatkan uang jutaan rupiah dipotong pajak',
+        //         'tanggal' => Carbon::now(),
+        //         'target_donasi' => 1000000,
+        //         'image_url' => 'awefiuih',
+        //     ],
+        //     [
+        //         'title' => 'Ojan Lomba 2 Title Semoga Menang',
+        //         'nama_lbh' => 'Ojanlomba 2',
+        //         'description' => 'Ojan 2 lagi lomba semoga dia bisa menang dan mendapatkan uang jutaan rupiah dipotong pajak',
+        //         'tanggal' => Carbon::now(),
+        //         'target_donasi' => 1000000,
+        //         'image_url' => 'awefiuih',
+        //     ],
+        //     [
+        //         'title' => 'Ojan Lomba 3 Title Semoga Menang',
+        //         'nama_lbh' => 'Ojanlomba 3',
+        //         'description' => 'Ojan 3 lagi lomba semoga dia bisa menang dan mendapatkan uang jutaan rupiah dipotong pajak',
+        //         'tanggal' => Carbon::now(),
+        //         'target_donasi' => 1000000,
+        //         'image_url' => 'awefiuih',
+        //     ],
+        // ];
+
+        // KasusHukum::insert($kasusHukumArray);
+        FormPengajuan::insert($formArray);
+        Bank::insert($bankArray);
+        LBH::insert($lbhArray);
         KasusHukum::insert($kasusHukumArray);
+        ProgressKasusHukum::insert($progressArray);
+        TransaksiDonasi::insert($transaksiArray);
     }
 }
