cbcccbd113eb60baedc09155f5d0ccde91866e5c
diandrakina <diandrapramestikinasih@gmail.com>
2024-07-15 13:22:05 +0700

    Memperbarui HomePage

--- resources/views/admin/perkara-pengajuan-detail.blade.php
+++ resources/views/admin/perkara-pengajuan-detail.blade.php
@@ -130,16 +130,28 @@
             </div>
         </div>
 
-        <div class="d-flex justify-content-center mt-2 gap-4 mb-5" style="color:rgba(232, 123, 47, 1)">
+        <div class="d-flex justify-content-center mt-3 gap-4 mb-4" style="color:rgba(232, 123, 47, 1)">
             <form action="{{ url('/lbh/pengajuan-bantuan-hukum/') }}" method="POST">
                 @csrf
                 @method('PUT')
                 <input type="hidden" name="id_form" value="">
-                <button type="submit">
-                    <i class="fa-solid fa-circle-check fa-4x"></i>
-                </button>
+                {{-- <button type="submit"> --}}
+                <div class="accept">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                        class="bi bi-check2" viewBox="0 0 16 16" stroke-width="3">
+                        <path
+                            d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0" />
+                    </svg>
+                </div>
+                {{-- </button> --}}
             </form>
-            <i class="fa-solid fa-circle-xmark fa-4x"></i>
+            <div class="deny">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                </svg>
+            </div>
         </div>
     </div>
 @endsection
